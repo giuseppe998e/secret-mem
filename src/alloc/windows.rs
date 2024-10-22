7461cdb8ba9aea5c3f482f9ef01540286f5d874b
Giuseppe Eletto <giuseppe@eletto.me>
2024-10-22 19:36:37 +0200

    feat: add windows alloc implementation

--- src/alloc/windows.rs
+++ src/alloc/windows.rs
@@ -1,7 +1,16 @@
-use core::{alloc::Layout, ptr::NonNull};
+use core::{
+    alloc::Layout,
+    ptr::{self, NonNull},
+};
 use std::io;
 
-use super::SecretAllocator;
+use windows_sys::Win32::System::Memory::{
+    self as windows, MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_NOCACHE, PAGE_READONLY,
+    PAGE_READWRITE,
+};
+use zeroize::Zeroize;
+
+use super::{util, SecretAllocator};
 
 // FIXME Current implementation wastes memory
 /// Provides an implementation of the `SecretAllocator` trait for Windows systems.
@@ -19,20 +28,80 @@ impl WindowsSecretAllocator {
 
 impl SecretAllocator for WindowsSecretAllocator {
     fn alloc(&self, layout: Layout) -> io::Result<NonNull<u8>> {
-        todo!()
+        let size = util::aligned_layout_size(&layout);
+
+        let virt_alloc = unsafe {
+            windows::VirtualAlloc(
+                ptr::null_mut(),
+                size,
+                MEM_COMMIT | MEM_RESERVE,
+                PAGE_READWRITE | PAGE_NOCACHE,
+            )
+        };
+
+        if virt_alloc.is_null() {
+            return Err(io::Error::last_os_error());
+        }
+
+        if unsafe { windows::VirtualLock(virt_alloc, size) } == 0 {
+            let last_error = io::Error::last_os_error();
+            unsafe { windows::VirtualFree(virt_alloc, 0, MEM_RELEASE) };
+            return Err(last_error);
+        }
+
+        let ptr = unsafe { NonNull::new_unchecked(virt_alloc as *mut _) };
+        Ok(ptr)
     }
 
     // NOTE Protection acts on an entire page, not a section.
     fn make_read_only(&self, ptr: NonNull<u8>, layout: Layout) -> io::Result<()> {
-        todo!()
+        let size = util::aligned_layout_size(&layout);
+        let prot_result = unsafe {
+            windows::VirtualProtect(
+                ptr.as_ptr() as *mut _,
+                size,
+                PAGE_READONLY,
+                (&mut 0u32) as *mut _,
+            )
+        };
+
+        match prot_result {
+            0 => Err(io::Error::last_os_error()),
+            _ => Ok(()),
+        }
     }
 
     // NOTE Protection acts on an entire page, not a section.
     fn make_writable(&self, ptr: NonNull<u8>, layout: Layout) -> io::Result<()> {
-        todo!()
+        let size = util::aligned_layout_size(&layout);
+        let prot_result = unsafe {
+            windows::VirtualProtect(
+                ptr.as_ptr() as *mut _,
+                size,
+                PAGE_READWRITE,
+                (&mut 0u32) as *mut _,
+            )
+        };
+
+        match prot_result {
+            0 => Err(io::Error::last_os_error()),
+            _ => Ok(()),
+        }
     }
 
     fn dealloc(&self, ptr: NonNull<u8>, layout: Layout) -> io::Result<()> {
-        todo!()
+        self.make_writable(ptr, layout)?;
+        let size = util::aligned_layout_size(&layout);
+
+        Zeroize::zeroize({
+            let bytes_slice = ptr::slice_from_raw_parts_mut(ptr.as_ptr(), size);
+            unsafe { &mut *bytes_slice }
+        });
+
+        unsafe { windows::VirtualUnlock(ptr.as_ptr() as *mut _, size) };
+        match unsafe { windows::VirtualFree(ptr.as_ptr() as *mut _, 0, MEM_RELEASE) } {
+            0 => Err(io::Error::last_os_error()),
+            _ => Ok(()),
+        }
     }
 }
